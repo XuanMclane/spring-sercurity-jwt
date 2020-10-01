376d2c30b11621ba61911587a4d73950c7178055
XuanMclane <xuanmclane@gmail.com>
2020-09-30 20:50:01 -0400

    Initial commit

---
+++ src/main/java/io/javabrains/springsercurityjwt/SpringSercurityJwtApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.springsercurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSercurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSercurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/springsercurityjwt/SpringSercurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springsercurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSercurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
